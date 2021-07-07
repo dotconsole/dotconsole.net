cdc781a7e40e7dabd8b768ec500d66cf990fb41d
Abdulhafiz Ahmed <abdulhafizahmed01@gmail.com>
2021-07-07 23:00:34 +0700

    Updates

--- components/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <img src="https://www.picpedia.org/highway-signs/images/under-construction.jpg" class="mx-auto" alt="under-construction"/>
-    <small class="text-sm w-3/4 mx-auto block"> image by 
-      <a href="http://www.nyphotographic.com/" target="_blank"> Nick Youngson</a> 
-      <a href="https://creativecommons.org/licenses/by-sa/3.0CC/" target="_blank"> BY-SA 3.0</a>
-      <a href="http://alphastockimages.com/" target="_blank"> Alpha Stock Images</a></small>
-  </div>
-</template>
